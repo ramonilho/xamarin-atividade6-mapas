875aaa6ae82360b2aafc41e3725069ca6c736d95
Ramon Honorio <ramonaqh@gmail.com>
2017-12-06 22:55:15 -0200

    initial commit

---
+++ XF.Mapas/XF.Mapas/App_Code/ListaMenu.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XF.Mapas.App_Code
+{
+    public class ListaMenu
+    {
+        public string Descricao { get; set; }
+        public string Icone { get; set; }
+        public Type TargetType { get; set; }
+    }
+}
